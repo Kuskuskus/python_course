26627914d98374bc3bcaf45a0246d3fd97a64634
Kozeeva Anna <kozeeva.an@yandex.ru>
2020-02-15 18:14:39 +0300

    files of lesson 1 were added

---
+++ info.py
@@ -0,0 +1,5 @@
+user_info = {"first_name":"Anna", "last_name":"Kozeeva"}
+print(user_info)
+print(user_info["first_name"])
+first_name = user_info["first_name"]
+print(first_name, user_info["first_name"], sep='**')
